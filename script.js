f6c86c298160ab09f14f68362f241ca527bb68fb
saharjamali76 <56353997+saharjamali76@users.noreply.github.com>
2022-06-04 20:38:51 +0430

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const day = "monday";
+if (day=="monday") {
+  console.log("Plan course structure");
+} else if (day=="sunday") {
+  console.log("prepare content");
+} else if (day=="thursday" ||day== "friday") {
+  console.log("record video");
+} else {
+  console.log("not a valid day");
+}
